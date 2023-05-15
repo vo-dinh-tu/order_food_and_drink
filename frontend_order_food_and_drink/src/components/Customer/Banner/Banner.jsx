c79eadcf5b384449265c1584fad04a19bac8c1b3
ThoThao <nguyenthothao1996@gmail.com>
2023-05-15 23:36:28 +0700

    Banner UI

--- frontend_order_food_and_drink/src/components/Customer/Banner/Banner.jsx
+++ frontend_order_food_and_drink/src/components/Customer/Banner/Banner.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
 import './banner.scss';
+import bannerImg from '../../../assets/img/banner.png';
 
 function Banner(props) {
     return (
-        <div className='block-banner'>
-            <h2>Banner</h2>
+        <div className='banner'>
+            <div className='banner__content'>
+                <h2 className='banner__content__title'>
+                    Thử ngay các món ăn trên TBayEAT
+                </h2>
+                <div className='banner__content__desc'>
+                    Đặt món trên TBayEAT để cảm nhận dịch vụ tiện ích mà chúng tôi mang lại cho bạn những món ăn ngon hấp dẫn.
+                </div>
+                <div className='banner__content__search-box'>
+                    <input type='text' value='' name='' placeholder='Tìm kiếm món ăn yêu thích'/>
+                    <div className='banner__content__btn search-btn'>Tìm kiếm</div>
+                </div>
+            </div>
+            <div className='banner__image'>
+                <img src={bannerImg} alt='' title='' />
+            </div>
         </div>
     );
 }
