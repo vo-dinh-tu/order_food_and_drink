c69a7edd8403b78b4372d44bf5b67a9cf8ba647a
tuvd <vodinhtu28@gmail.com>
2023-06-03 04:11:14 +0700

    add socket process

--- frontend_order_food_and_drink/src/pages/Customer/HistoryOrder/HistoryOrder.jsx
+++ frontend_order_food_and_drink/src/pages/Customer/HistoryOrder/HistoryOrder.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from "react";
+import socketIOClient from "socket.io-client";
+const host = "http://localhost:3000";
 
 function HistoryOrder(props) {
+    const [order, setOrder] = useState([]);
+    const socketRef = useRef();
+    const user = JSON.parse(sessionStorage.getItem("user"));
+
+    useEffect(() => {
+        socketRef.current = socketIOClient.connect(host);
+        socketRef.current.emit('userConnect', user.id);
+        socketRef.current.on('sendStatusOrder', dataOrder => {
+            console.log(dataOrder);
+            setOrder(dataOrder);
+        });
+    }, []);
+
     return (
         <div>
             <h2>History order</h2>
